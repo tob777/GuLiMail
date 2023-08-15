f3e78bbe029ef675fa68e3635b4be88387a2b29c
chenzhirui <303266726@qq.com>
2023-08-15 17:02:29 +0800

    project initialization

---
+++ gulimail-wave/src/main/java/com/atguigu/gulimail/wave/GulimailWaveApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimail.wave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimailWaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimailWaveApplication.class, args);
+    }
+
+}
---
+++ gulimail-wave/src/test/java/com/atguigu/gulimail/wave/GulimailWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimail.wave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailWaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
